fff3ee2fca57f2cab3441239ec051a34bb633aa6
omid.sed <osedighimornani@gmail.com>
2026-05-26 11:29:20 +0200

    feat: improved interface for backend pipeline tasks
    - CaseFileDocument as Output of OCR-reading
    -SegmentationResult as Output of segmentation
    - README for the backend to explain pipeline

--- backend/reader.py
+++ backend/reader.py
@@ -1,6 +1,11 @@
 import io
-from datatypes import File
+from datatypes import CaseFileDocument, ExtracedPageError, PageContent
 
 # Korbi
-def read_document(file: io.BytesIO) -> File:
+
+# Ein paar Ansätze, ist aber selbst überlassen: (vlt. auch kein BytesObjekt als Input sondern file name als string direkt, etc.)
+def read_document(file: io.BytesIO) -> CaseFileDocument:
+  pass
+
+def _extract_page(page) -> PageContent | ExtracedPageError :
   pass
