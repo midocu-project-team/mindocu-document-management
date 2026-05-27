9b5fd9ba606ce71c227c7095078fcbcd39113fed
DanielGruberTB <daniel-gruber@outlook.de>
2026-05-27 18:22:00 +0200

    Added sidebar component

---
+++ frontend/mindocu_webpage/src/components/sidebar.tsx
@@ -0,0 +1,11 @@
+export function Sidebar() {
+  return (
+    <section id="sidebar">
+      <h1>Logo</h1>
+      <nav>
+        <a href="/">Dashboard</a>
+        <a href="/documents">Documents</a>
+      </nav>
+    </section>
+  )
+}
