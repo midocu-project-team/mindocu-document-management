e71d4edbb9caee681e9e82d57676b7237e2f4d61
DanielGruberTB <daniel-gruber@outlook.de>
2026-05-27 18:02:35 +0200

    added react + vite project and added material ui

---
+++ frontend/mindocu_webpage/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div id="layout">
+      <section id="sidebar">
+        <h1>Sidebar</h1>
+      </section>
+      <section id="content">
+        <h1>Center</h1>
+      </section>
+    </div>
+  )
+}
+
+export default App
