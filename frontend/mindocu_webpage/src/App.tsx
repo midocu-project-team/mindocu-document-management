03f91e263cb9922bfaec668d3ec9212b263af9fc
DanielGruberTB <daniel-gruber@outlook.de>
2026-06-10 15:17:14 +0200

    fix: merge problem with duplicate line resolved by removing duplicate line

--- frontend/mindocu_webpage/src/App.tsx
+++ frontend/mindocu_webpage/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import MainLayout from './layouts/MainLayout'
 import CaseHomePage from './pages/CaseHomePage'
